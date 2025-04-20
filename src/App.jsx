cad8fd56fa87dfdee8b20c4d35fbab64cf0015ec
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-20 14:20:27 +0100

    [feature] added loading and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col'>
+      <div className='flex flex-col justify-center items-center'>
         <div>
           <a href="https://vite.dev" target="_blank">
             <img src={viteLogo} className="logo" alt="Vite logo" />
