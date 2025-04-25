dfebfe66ea1a85f92319a3e0440fad1135d37d9c
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-25 17:25:07 +0100

    [tweak] did some cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -12,33 +12,12 @@ function App() {
   return (
     <>
       <div className='flex flex-col justify-center items-center'>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
         <div className="grid grid-cols-2">
-        <div><h1>Vite + React</h1></div><div className='flex items-center justify-center scale-125'><Animation1/></div>
+          <div><h1>GloNeuro</h1></div><div className='flex items-center justify-center scale-125'><Animation1 /></div>
         </div>
-        
-        <div>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Test <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-          <div class="scroll-down"></div>
-        </p>
       </div>
-      
 
+      <div class="scroll-down"></div>
 
       <div>
         <Posts />
