e643af3c87edcdf022a63fe5039c88216dd5c2f9
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-27 19:07:06 +0100

    [tweak] aesthetic tweaks dark mode

--- src/assets/components/ThemeSelector.jsx
+++ src/assets/components/ThemeSelector.jsx
@@ -20,7 +20,7 @@ const ThemeSelector = ({ theme, setTheme }) => {
             <div className="inline-block relative">
                 {/* Button */}
                 <button
-                    className="px-4 py-2 bg-gray-300 dark:bg-gray-700 text-black dark:text-white rounded-lg flex items-center"
+                    className="px-4 py-2 !bg-white dark:!bg-black !text-black dark:!text-white rounded-lg flex items-center"
                 >
                     {theme === 'system' ? <GrSystem /> : theme === 'light' ? <MdOutlineLightMode /> : <MdDarkMode />}
                     <span className="ml-2">
@@ -34,24 +34,24 @@ const ThemeSelector = ({ theme, setTheme }) => {
     absolute mt-2 left-1/2 transform -translate-x-1/2 
     min-w-[12rem] bg-gray-300 dark:bg-gray-700 rounded-lg shadow-lg 
     transition-all duration-300 ease-in-out z-50
-    ${open ? 'opacity-100 translate-y-0 scale-100 pointer-events-auto' : 'opacity-0 translate-y-2 scale-95 pointer-events-none'}
+    ${open ? 'opacity-80 backdrop-blur-6xl translate-y-0 scale-100 pointer-events-auto' : 'opacity-0 translate-y-2 scale-95 pointer-events-none'}
   `}
                 >
                     <button
                         onClick={() => { setTheme('system'); setOpen(false); }}
-                        className="w-full flex items-center justify-center gap-2 px-4 py-2 text-black dark:text-white hover:bg-gray-400 dark:hover:bg-gray-600 rounded-t-lg"
+                        className="w-full flex items-center justify-center gap-2 px-4 py-2 !text-black dark:!text-white dark:!bg-black hover:!outline-2 hover:!outline-indigo-500 hover:!bg-gray-400 dark:hover:!bg-gray-600 rounded-t-lg"
                     >
                         <GrSystem /> System Default
                     </button>
                     <button
                         onClick={() => { setTheme('light'); setOpen(false); }}
-                        className="w-full flex items-center justify-center gap-2 px-4 py-2 text-black dark:text-white hover:bg-gray-400 dark:hover:bg-gray-600"
+                        className="w-full flex items-center justify-center gap-2 px-4 py-2 !text-black dark:!text-white dark:!bg-black hover:!outline-2 hover:!outline-indigo-500 hover:!bg-gray-400 dark:hover:!bg-gray-600"
                     >
                         <MdOutlineLightMode /> Light Mode
                     </button>
                     <button
                         onClick={() => { setTheme('dark'); setOpen(false); }}
-                        className="w-full flex items-center justify-center gap-2 px-4 py-2 text-black dark:text-white hover:bg-gray-400 dark:hover:bg-gray-600 rounded-b-lg"
+                        className="w-full flex items-center justify-center gap-2 px-4 py-2 !text-black dark:!text-white dark:!bg-black hover:!outline-2 hover:!outline-indigo-500 hover:!bg-gray-400 dark:hover:!bg-gray-600 rounded-b-lg"
                     >
                         <MdDarkMode /> Dark Mode
                     </button>
